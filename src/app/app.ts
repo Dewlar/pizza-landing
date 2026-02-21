902cacbae181d9f7bf9ee501b0ec0a4288d4c3ca
Andrei Markouski <insawm@gmail.com>
2026-02-21 16:59:21 +0300

    chore: settings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HomePageComponent } from './features/pages/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomePageComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {
-  protected readonly title = signal('pizza-landing');
-}
+export class App {}
