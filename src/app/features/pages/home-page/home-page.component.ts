f667c4cee6fd3cd2e01660dcc0c3e516fff46616
Andrei Markouski <insawm@gmail.com>
2026-02-22 23:19:25 +0300

    feat: add promo section

--- src/app/features/pages/home-page/home-page.component.ts
+++ src/app/features/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { PromoSectionComponent } from '../../components/promo-section/promo-section.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, PromoSectionComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
